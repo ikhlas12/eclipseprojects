7b1c51bc1948e2762744071f1c659ed1bf3b0db5
Asus <Asus@pc-asus>
2020-05-28 16:01:53 +0200

    First commit Uploading all files

---
+++ FirstProjectUploaded/src/demo/Alien.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Alien {
+
+	
+	public static void main(String args[]) {
+		
+		
+		
+	}
+}
---
+++ FirstProjectUploaded/src/demo/Calculator.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+
+		return a+b;
+
+	}
+}
